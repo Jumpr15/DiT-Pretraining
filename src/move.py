56173286ae7d4f0fac6d0076df73f1f720ed6f27
Jumpr15 <chordataosteichthyes@gmail.com>
2026-05-07 10:09:17 +1000

    added dir move script

---
+++ src/move.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+#Path of source directory & destination directory
+src_directory = '/danbooru_faces/danbooru_eva_faces'
+dst_directory = '/images'
+
+# Extract file from Source directory and copy to Destination directory
+for file_num in range(4):   
+     for file in os.listdir(f'/danbooru_faces/danbooru_eva_faces/{file_num}'):
+          src_file = os.path.join(src_directory, file)
+          dest_file = os.path.join(dst_directory, file)
+
+          shutil.copy(src_file, dest_file)
